2d4437567b5dcafadb79b82f125c412f5e341177
Nishant Koli <nishant.coolie@gmail.com>
2017-07-13 16:32:01 -0400

    First commit

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,26 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
-
-  }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
-
+  .controller('ProductsCtrl', ['$scope','$stateParams', function($scope, $stateParams) {
+      $scope.category = $stateParams.category
+      $scope.productsListing = [{
+          product_id: '123',
+          title: ' Vintage USA Navy Ship model ',
+          price: 120,
+          userName: 'John Doe'
+      },  {
+          product_id: '456',
+          title: ' Rustic miniature chair set',
+          price: 100,
+          userName: 'Sandy Peters'
+      }
+  ]}])
+  .controller('ProductDetailsCtrl', ['$scope','$stateParams', function($scope, $stateParams) {
+      $scope.id = $stateParams.id;
+      $scope.product = {
+          'title': 'Vintage USA Navy Ship model',
+          'description': 'A model ship of the actual 1924 USA navy ship',
+          'price': 120,
+          'userName': 'John Doe'
+      }
   }]);
